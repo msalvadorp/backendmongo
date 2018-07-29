5477eb559c78c6ba1c0ed621052fc4dcc5617224
Miguel Salvador <msalvadorp@hotmail.com>
2018-07-29 08:33:10 -0500

    Fin Hospitales capitulo 11

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -1,13 +1,25 @@
 var express = require('express')
-
+const fs = require("fs")
+const path = require("path")
 const app = express()
 
-app.get("/", (req, res, next)=>{
-    res.status(200).json({
-        ok : true,
-        mensaje : "Dato correcto corregido 3"
+app.get("/:tipo/:img", (req, res, next)=>{
 
-    })
+    //falta agregar las validaciones de la carpeta
+    let tipo = req.params.tipo
+    let img = req.params.img
+
+    let ruta = `./uploads/${ tipo }/${ img }`
+    let pathImagen = path.resolve(__dirname, `../uploads/${ tipo }/${ img }`)
+
+    if (fs.existsSync(pathImagen)){
+        return res.sendFile(pathImagen)
+    }
+    else {
+        let pathNoImage = path.resolve(__dirname, "../assets/no-img.jpg")
+        return res.sendFile(pathNoImage)
+    }
+
 })
 
 module.exports = app
