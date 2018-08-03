392f940be893c6eb43ec94731acf176a1412e22c
msalvadorp <msalvadorp@hotmail.com>
2018-08-03 18:02:53 -0500

    modificacion para cors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ var bodyParser = require("body-parser")
 //const app : Application  = express()
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
